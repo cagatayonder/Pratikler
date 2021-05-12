58b70f4924d3b0fa4e5b9f135ab8871b6e4fb869
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-12 18:38:50 +0300

    operatorler

---
+++ operatörler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace operatörler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x =3;
+            int y =5;
+
+            y += 2;
+            x *= 3;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            bool isSuccess = true;
+            bool isCompleted = true;
+
+            if(isSuccess&&isCompleted)
+            Console.WriteLine("Helal");
+            
+
+        }
+    }
+}
