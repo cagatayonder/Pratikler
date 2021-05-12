768f032b84a375d8549420c1481732415c2a42e9
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-12 17:35:59 +0300

    Final

---
+++ console-programlama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your Name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello "+ name);
+        }
+    }
+}
