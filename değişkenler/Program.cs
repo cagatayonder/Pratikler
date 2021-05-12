0224d6a6e3a60d83f4f5a7478a15d3c512fe6c06
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-12 18:15:11 +0300

    Final

---
+++ değişkenler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace değişkenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+              Console.WriteLine(datetime);
+        }
+    }
+}
