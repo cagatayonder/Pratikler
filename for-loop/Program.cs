66944736664c64df1f2d7a91403c275c56e3cdbd
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-13 11:25:20 +0300

    For-Loop

---
+++ for-loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı Giriniz");
+            int sayac =  int.Parse(Console.ReadLine());
+            for ( int i=0; i<= sayac; i++){
+                if(i%2 ==1){
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
