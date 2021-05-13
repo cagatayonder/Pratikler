532ab730da8d879e7529b0323400917c70584508
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-13 11:14:06 +0300

    Debug-Watch

---
+++ debug-watch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace debug_watch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            try{Console.WriteLine("Write a Number");
+            int deger = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı " +deger);
+            }
+            catch(Exception ex){
+                Console.WriteLine("Hata" +ex.Message.ToString());
+            }
+            finally{
+                Console.WriteLine("İşlem Tamamlandı!");
+            }
+
+        }
+            
+    }
+}
+       
