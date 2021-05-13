1f41b47904bf516d3c00d3735d2829228fdd6ca4
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-13 10:24:16 +0300

    hata-yonetimi

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{Console.WriteLine("Write a Number");
+            int deger = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı " +deger);
+            }
+            catch(Exception ex){
+                Console.WriteLine("Hata" +ex.Message.ToString());
+            }
+            finally{
+                Console.WriteLine("İşlem Tamamlandı!");
+            }
+
+        }
+            
+    }
+}
