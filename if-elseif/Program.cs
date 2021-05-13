a26870bec6660db375ad57c6563ff51a0c26c8b4
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-13 10:34:28 +0300

    Ternary/if

---
+++ if-elseif/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_elseif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            // if-else-elseif
+            if(time>= 6 && time<18){
+                Console.WriteLine("Günaydın");
+            }
+
+            else if( time <= 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else{
+                Console.WriteLine("İyi Geceler");
+            }
+            // Ternary 
+
+            // string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler";
+
+            string sonuc = time>= 6 && time<18 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
