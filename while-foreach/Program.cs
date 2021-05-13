a219780b45cd103042d6297d2c00c9073d3d8c7b
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-13 11:42:29 +0300

    While-Foreach

---
+++ while-foreach/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayı Giriniz");
+            int sayı = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+
+            while(sayac <= sayı){
+
+                toplam += sayac;
+                sayac++;
+
+            }
+            Console.WriteLine(toplam/sayı);
+
+            Console.Write("******** Foreach *****************");
+            Console.WriteLine();
+            string[] arabalar = {"BMW","Ford","Fiat"};
+
+            foreach ( var araba in arabalar){
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
