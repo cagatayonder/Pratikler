06f6eaf013f7fabcc5c109c86528090c059f46e6
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-13 12:20:16 +0300

    Diziler

---
+++ diziler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5] ;
+
+            string[] hayvanlar = {"Kedi","Köpek","Kuş","Balık"};
+
+            // Console.WriteLine(hayvanlar.Length);
+
+            renkler[0] = "Mavi";
+
+            Console.WriteLine("Bir sayı giriniz.");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayıDizisi = new int[diziUzunlugu];
+
+            for( int i= 0; i<diziUzunlugu ; i++){
+                Console.WriteLine("Lutfen {0}. sayısı giriniz", i+1);
+                sayıDizisi[i] = int.Parse(Console.ReadLine());
+            }
+            int toplam = 0;
+            foreach (var sayı in sayıDizisi)
+            {
+                toplam += sayı;
+            }
+            Console.WriteLine("Ortalama" +(toplam/diziUzunlugu));
+        }
+    }
+}
