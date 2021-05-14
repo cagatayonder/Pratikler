997990a560f695d5e8d831990fba6dfa0ea2cb13
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-14 13:36:34 +0300

    kurucu

---
+++ kurucu_methods/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace kurucu_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Calısan calısan1 = new Calısan("Çağatay"," Önder",12345678,"Backend");
+            
+            Calısan calısan2 = new Calısan("Recep"," Keçeci",12345679,"Game-Dev");
+
+            Calısan calısan3 = new Calısan("Büşra"," Kandemir");
+
+            calısan1.CalısanBilgileri();
+            calısan2.CalısanBilgileri();
+            calısan3.CalısanBilgileri();
+        }
+    }
+
+    class Calısan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+        public Calısan( string ad, string soyad, int no, string departman){
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.No = no;
+            this.Departman = departman;
+
+        }
+        public Calısan( string ad, string soyad){
+            this.Ad = ad;
+            this.Soyad = soyad;
+        }
+        public Calısan(){}
+        public void CalısanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adı:{0}",Ad);
+            Console.WriteLine("Çalışan Soyadı:{0}",Soyad);
+            Console.WriteLine("Çalışan Numarası:{0}",No);
+            Console.WriteLine("Çalışan Departmanı:{0}",Departman);
+        }
+
+        
+    }
+}
