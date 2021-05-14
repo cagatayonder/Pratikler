cfd735560ab5f9ff96774f60ee8242b70b0c6429
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-14 14:29:48 +0300

    static_

---
+++ static/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace static_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Çalışan Sayısı:{0}",Calısan.CalısanSayısı);
+
+            Calısan calısan1 = new Calısan("Çağatay","Onder","Backend");
+            Console.WriteLine("Çalışan Sayısı:{0}",Calısan.CalısanSayısı);
+
+            Calısan calısan2 = new Calısan("REcep","Kecei","Game-DEv");
+            Console.WriteLine("Çalışan Sayısı:{0}",Calısan.CalısanSayısı);
+        }
+    }
+
+    class Calısan 
+    {
+        private static int calısanSayısı;
+
+        public static int CalısanSayısı { get => calısanSayısı; set => calısanSayısı = value; }
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+        static Calısan(){
+            calısanSayısı = 0;
+        }
+
+        public Calısan(string ısim, string soyisim, string departman){
+            this.Isim = ısim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calısanSayısı ++;
+        }
+    }
+}
