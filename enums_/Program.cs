5014f3863488c28c6179c2c1670b6f0889ddd745
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-05-14 17:57:42 +0300

    enums_

---
+++ enums_/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace enums_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine(Günler.Pazar);
+        }
+    }
+    enum Günler
+    {
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+}
